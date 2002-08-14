52f57d67e1db04b68958441a3f91ca25f708f40a
mschwendt <>
2002-08-14 07:27:13 +0000

    Added for src/Config.h.in and autoheader.

---
+++ xmms-fc/acconfig.h
@@ -0,0 +1,13 @@
+/* Define if your compiler supports type ``bool''.
+   If not, a user-defined signed integral type will be used.  */
+#undef FC_HAVE_BOOL
+
+/* Define if standard member ``ios::binary'' is available. */
+#undef FC_HAVE_IOS_BINARY
+
+/* Define if ``nothrow allocator'' is available.  */
+#undef FC_HAVE_NOTHROW
+
+/* Define only if old non-standard C++ headers are available.  */
+#undef FC_HAVE_OLD_CPP_HEADERS
+
