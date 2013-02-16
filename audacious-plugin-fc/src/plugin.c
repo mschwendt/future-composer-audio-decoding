9cebee7326dbd2e8eb05ccab279b3025ac03b586
mschwendt <>
2013-02-16 18:05:30 +0000

    updates / get rid of GTK+ usage

--- audacious-plugin-fc/src/plugin.c
+++ audacious-plugin-fc/src/plugin.c
@@ -1,5 +1,11 @@
 #include <glib.h>
 #include <audacious/plugin.h>
+#include "config.h"
+
+static const char fc_ip_about[] =
+   "Future Composer decoder plug-in\n\n"
+   "http://xmms-fc.sourceforge.net\nCreated by Michael Schwendt.\n\n"
+   "Much room for improvement.\nHave a go if you like to.\n";
 
 const gchar* const fc_fmts[] = { "fc", "fc13", "fc14", NULL };
 
@@ -7,7 +13,6 @@ extern PluginPreferences fc_ip_preferences;
 
 gboolean ip_init(void);
 void ip_cleanup(void);
-void fc_ip_about(void);
 gint ip_is_valid_file_vfs(const gchar *filename, VFSFile *fd);
 gboolean ip_play(InputPlayback *playback, const gchar *filename, VFSFile *fd,
                  gint start_time, gint stop_time, gboolean pause);
@@ -19,9 +24,9 @@ Tuple *ip_probe_for_tuple(const gchar *filename, VFSFile *fd);
 
 AUD_INPUT_PLUGIN
 (
-    .name = "Future Composer decoder",
+    .name = "Future Composer Decoder " VERSION,
     .init = ip_init,
-    .about = fc_ip_about,
+    .about_text = fc_ip_about,
     .prefs = &fc_ip_preferences,
     .have_subtune = FALSE,
     .extensions = fc_fmts,
